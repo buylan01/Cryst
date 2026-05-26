a3c36c7ed290d3db84ed0b997ef2873400688852
buylan <buylan01@gmail.com>
2026-05-26 23:47:26 +0800

    Fix: Resolution issue in imageViewer

--- app/src/main/java/com/buylan/cryst/ui/screen/imageviewer/ImageViewer.kt
+++ app/src/main/java/com/buylan/cryst/ui/screen/imageviewer/ImageViewer.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.FormatColorFill
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,6 +24,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -101,17 +103,21 @@ fun ImageViewer(
     ) {
         Box(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
+            var loaded by remember { mutableStateOf(false) }
             var imageSize by remember { mutableStateOf(Size(100f, 100f)) }
             val builder = ImageRequest.Builder(LocalContext.current)
                 .data(file)
+                .size(coil.size.Size.ORIGINAL)
                 .listener(
                     onSuccess = { _, result ->
                         imageSize = Size(
                             width = result.drawable.intrinsicWidth.toFloat(),
                             height = result.drawable.intrinsicHeight.toFloat()
                         )
+                        loaded = true
                     }
                 )
                 .build()
@@ -124,6 +130,7 @@ fun ImageViewer(
                     modifier = Modifier.fillMaxSize()
                 )
             }
+            if (!loaded) CircularProgressIndicator()
         }
     }
 }
